f7ff8753c70a625c3631d40f3922cfbb4bfd03b4
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-03-27 23:04:46 +0100

    updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -25,3 +25,30 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self, key: str, fn: Callable = None
+            ) -> Union[str, bytes, int, None]:
+        """
+        get method
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        get_str method
+        """
+        return self.get(
+            key, fn=lambda d: d.decode("utf-8") if d is not None else None
+                        )
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        get_int method
+        """
+        return self.get(key, fn=lambda d: int(d) if d is not None else None)
